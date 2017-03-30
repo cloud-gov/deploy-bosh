74f8fcf57c4f31e5576fde51efde8dd3bea87e02
Joshua Carp <jm.carp@gmail.com>
2017-03-30 18:23:48 -0400

    Use instance profiles instead of IAM users.

--- generate.sh
+++ generate.sh
@@ -2,9 +2,9 @@
 
 set -e -x
 
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+SCRIPTPATH=$(cd $(dirname $0); pwd -P)
 
 spruce merge \
-  --prune meta --prune templates \
-  $SCRIPTPATH/bosh-deployment.yml \
-  $@
+  --prune meta --prune templates --prune terraform_outputs \
+  "$SCRIPTPATH/bosh-deployment.yml" \
+  "$@"
