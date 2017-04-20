5d2fa68bb4be22bccdae70167155d91a73c42e3b
Joshua Carp <jm.carp@gmail.com>
2017-04-20 19:52:47 -0400

    Add admin-ui config.

--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -4,9 +4,10 @@ set -e -u
 
 bosh-cli login
 
-spruce merge --prune terraform_outputs \
-  cloud-config.yml \
-  terraform-yaml/state.yml \
-  > cloud-config-final.yml
+files=("bosh-config/cloud-config/cloud-config-base.yml" "terraform-yaml/state.yml")
+if [ -n "${MANIFEST_PATH:-}" ]; then
+  files=(${files[@]} "${MANIFEST_PATH}")
+fi
+spruce merge --prune terraform_outputs "${files[@]}" > cloud-config-final.yml
 
 bosh-cli update-cloud-config cloud-config-final.yml
