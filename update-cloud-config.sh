73ff3e9efe34bb333ea41bb06f2e82ba61648568
Joshua Carp <jm.carp@gmail.com>
2017-04-22 15:08:26 -0400

    Handle basic auth.

--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -2,7 +2,14 @@
 
 set -e -u
 
-bosh-cli login
+if [ -n "${BOSH_USERNAME:-}" ]; then
+  # Hack: Add trailing newline to skip OTP prompt
+  bosh-cli login <<EOF 1>/dev/null
+${BOSH_USERNAME}
+${BOSH_PASSWORD}
+
+EOF
+fi
 
 files=("bosh-config/cloud-config/cloud-config-base.yml" "terraform-yaml/state.yml")
 if [ -n "${MANIFEST_PATH:-}" ]; then
@@ -10,4 +17,4 @@ if [ -n "${MANIFEST_PATH:-}" ]; then
 fi
 spruce merge --prune terraform_outputs "${files[@]}" > cloud-config-final.yml
 
-bosh-cli update-cloud-config cloud-config-final.yml
+bosh-cli -n update-cloud-config cloud-config-final.yml
