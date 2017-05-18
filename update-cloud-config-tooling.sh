f52ec823971e19e4ec65d5410088cadf671cc163
Joshua Carp <jm.carp@gmail.com>
2017-05-18 17:19:48 -0400

    Deploy bosh using cloud config.

---
+++ update-cloud-config-tooling.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -u
+
+files=("bosh-config/cloud-config/cloud-config-base.yml" "terraform-yaml/state.yml")
+if [ -n "${MANIFEST_PATH:-}" ]; then
+  files=(${files[@]} "${MANIFEST_PATH}")
+fi
+
+for environment in "development" "staging" "production"; do
+  ENVIRONMENT=${environment} spruce merge --prune terraform_outputs \
+    bosh-config/cloud-config/cloud-config-bosh.yml \
+    terraform-yaml-${environment}/state.yml \
+    > ${environment}-bosh.yml
+  files=(${files[@]} ${environment}-bosh.yml)
+done
+
+spruce merge --prune terraform_outputs "${files[@]}" > cloud-config-final.yml
+
+# TODO delete after https://github.com/cloudfoundry/bosh-cli/issues/7 is resolved
+diff --old-group-format=$'\e[0;31m%<\e[0m' --new-group-format=$'\e[0;32m%>\e[0m' <(bosh-cli cloud-config) cloud-config-final.yml || true
+
+bosh-cli -n update-cloud-config cloud-config-final.yml
