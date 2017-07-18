70c95a177f479356e7ca00e0080f0df529502597
Chris Nelson <cnelson@cnelson.org>
2017-07-18 07:56:19 -0700

    Drop the diff as bosh v2 handles this now

--- update-cloud-config-tooling.sh
+++ update-cloud-config-tooling.sh
@@ -17,7 +17,4 @@ done
 
 spruce merge --prune terraform_outputs "${files[@]}" > cloud-config-final.yml
 
-# TODO delete after https://github.com/cloudfoundry/bosh-cli/issues/7 is resolved
-diff --old-group-format=$'\e[0;31m%<\e[0m' --new-group-format=$'\e[0;32m%>\e[0m' <(bosh-cli cloud-config) cloud-config-final.yml || true
-
 bosh-cli -n update-cloud-config cloud-config-final.yml
--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -8,7 +8,4 @@ for file in ${MANIFEST_PATH:-}; do
 done
 spruce merge --prune terraform_outputs "${files[@]}" > cloud-config-final.yml
 
-# TODO delete after https://github.com/cloudfoundry/bosh-cli/issues/7 is resolved
-diff --old-group-format=$'\e[0;31m%<\e[0m' --new-group-format=$'\e[0;32m%>\e[0m' <(bosh-cli cloud-config) cloud-config-final.yml || true
-
 bosh-cli -n update-cloud-config cloud-config-final.yml
