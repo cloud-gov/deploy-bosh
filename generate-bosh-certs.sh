04ccf62aa25087c120a44c8d96e8692a1e9a561b
M. Adam Kendall <michael.kendall@gsa.gov>
2017-12-21 12:20:53 -0500

    Update cert generation for UAA using bosh DNS for CN

--- generate-bosh-certs.sh
+++ generate-bosh-certs.sh
@@ -9,32 +9,34 @@ fi
 
 export BOSH_NAME=$1
 export BOSH_IP=$2
+ENV=${BOSH_NAME//-bosh/}
+export UAA_DOMAIN=${3:-"*.uaa.$ENV-bosh.${ENV}bosh.toolingbosh"}
 
 export TARGET="out"
 
 # generate a cert for the director
-certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}-bosh-director" --ip ${BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign ${BOSH_NAME}-bosh-director --CA master-bosh --passphrase ''
+certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}-bosh-director" --ip "${BOSH_IP}" --passphrase ''
+certstrap --depot-path ${TARGET} sign "${BOSH_NAME}-bosh-director" --CA master-bosh --passphrase ''
 
 # generate a saml signing cert
-certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}-uaa-saml" --ip ${BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign ${BOSH_NAME}-uaa-saml --CA master-bosh --passphrase ''
+certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}-uaa-saml" --domain "${UAA_DOMAIN}" --passphrase ''
+certstrap --depot-path ${TARGET} sign "${BOSH_NAME}-uaa-saml" --CA master-bosh --passphrase ''
 
 # generate a cert for UAA in ssl mode
-certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}-uaa-web" --ip ${BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign ${BOSH_NAME}-uaa-web --CA master-bosh --passphrase ''
+certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}-uaa-web" --domain "${UAA_DOMAIN},localhost" --passphrase ''
+certstrap --depot-path ${TARGET} sign "${BOSH_NAME}-uaa-web" --CA master-bosh --passphrase ''
 
 # extract the public key for this cert as right now we use it to sign JWTs as well
-openssl rsa -in ${TARGET}/${BOSH_NAME}-uaa-web.key -pubout > ${TARGET}/${BOSH_NAME}-pub.key
+openssl rsa -in "${TARGET}/${BOSH_NAME}-uaa-web.key" -pubout > "${TARGET}/${BOSH_NAME}-pub.key"
 
 # generate a cert for nats server
-certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}.nats.bosh-internal" --ip ${BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign ${BOSH_NAME}.nats.bosh-internal -CA master-bosh --passphrase ''
+certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}.nats.bosh-internal" --ip "${BOSH_IP}" --passphrase ''
+certstrap --depot-path ${TARGET} sign "${BOSH_NAME}.nats.bosh-internal" -CA master-bosh --passphrase ''
 
 # generate a cert for nats director
-certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}.director.bosh-internal" --ip ${BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign ${BOSH_NAME}.director.bosh-internal --CA master-bosh --passphrase ''
+certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}.director.bosh-internal" --ip "${BOSH_IP}" --passphrase ''
+certstrap --depot-path ${TARGET} sign "${BOSH_NAME}.director.bosh-internal" --CA master-bosh --passphrase ''
 
 # generate a cert for nats health monitor
-certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}.hm.bosh-internal" --ip ${BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign ${BOSH_NAME}.hm.bosh-internal --CA master-bosh --passphrase ''
+certstrap --depot-path ${TARGET} request-cert --cn "${BOSH_NAME}.hm.bosh-internal" --ip "${BOSH_IP}" --passphrase ''
+certstrap --depot-path ${TARGET} sign "${BOSH_NAME}.hm.bosh-internal" --CA master-bosh --passphrase ''
