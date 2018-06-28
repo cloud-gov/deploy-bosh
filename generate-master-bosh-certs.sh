2f1e4c36253e7429949633a2a6fc5ac15431eb89
Joshua Carp <jm.carp@gmail.com>
2018-06-27 21:40:05 -0400

    Drop outdated docs.

--- generate-master-bosh-certs.sh
+++ generate-master-bosh-certs.sh
@@ -22,20 +22,3 @@ rm ${TMPKEY}
 key_name="masterbosh-$(date +'%Y%m%d%H%M%S')"
 aws ec2 import-key-pair --key-name "${key_name}" --public-key-material "$(cat ${TARGET}/master-bosh.pub)"
 echo "${key_name}" > ./key-name
-
-# generate a key and cert for master-bosh nginx
-# note: it's important to specify a CN here and an IP, else certstrap with not add a SAN field which the new golang bosh cli requires
-certstrap --depot-path ${TARGET} request-cert --cn 'master-bosh-director' --ip ${MASTER_BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign master-bosh-director --CA master-bosh --passphrase ''
-
-# generate a cert for nats server
-certstrap --depot-path ${TARGET} request-cert --cn "master-bosh.nats.bosh-internal" --ip ${MASTER_BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign master-bosh.nats.bosh-internal -CA master-bosh --passphrase ''
-
-# generate a cert for nats director
-certstrap --depot-path ${TARGET} request-cert --cn "master-bosh.director.bosh-internal" --ip ${MASTER_BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign master-bosh.director.bosh-internal --CA master-bosh --passphrase ''
-
-# generate a cert for nats health monitor
-certstrap --depot-path ${TARGET} request-cert --cn "master-bosh.hm.bosh-internal" --ip ${MASTER_BOSH_IP} --passphrase ''
-certstrap --depot-path ${TARGET} sign master-bosh.hm.bosh-internal --CA master-bosh --passphrase ''
