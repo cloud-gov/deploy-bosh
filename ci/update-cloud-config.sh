71abcf9bef341fa1a6d71fddf3dbff0d53351939
Joshua Carp <jm.carp@gmail.com>
2018-07-19 16:43:46 -0400

    Manage cloud config with opsfiles instead of spruce.

--- ci/update-cloud-config.sh
+++ ci/update-cloud-config.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
-files=("bosh-config/cloud-config/base.yml" "terraform-yaml/state.yml")
-for file in ${MANIFEST_PATH:-}; do
-  files=(${files[@]} "${file}")
+args=("--vars-file" "terraform-yaml/state.yml")
+for ops in ${OPS_PATHS:-}; do
+  args=(${args[@]} "--ops-file" "${ops}")
 done
-spruce merge --prune terraform_outputs "${files[@]}" > cloud-config-final.yml
 
-bosh-cli -n update-cloud-config cloud-config-final.yml
+bosh -n update-cloud-config bosh-config/cloud-config/base.yml "${args[@]}"
