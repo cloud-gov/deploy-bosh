350ccf2d5bef7872c0e7c8df36f69c06004a3105
ChrisMcGowan <christopher.r.mcgowan@gsa.gov>
2021-12-14 12:14:42 -0500

    adding nessus-agent to master bosh

--- bosh-create-env.sh
+++ bosh-create-env.sh
@@ -24,6 +24,7 @@ bosh create-env \
   --ops-file bosh-config/operations/encryption.yml \
   --ops-file bosh-config/operations/add-cloud-gov-root-certificate.yml \
   --ops-file bosh-config/operations/masterbosh-ntp.yml \
+  --ops-file bosh-config/operations/add-nessus-agent.yml \
   --vars-file bosh-config/variables/master.yml \
   --vars-file terraform-yaml/state.yml \
   --vars-file terraform-secrets/terraform.yml \
